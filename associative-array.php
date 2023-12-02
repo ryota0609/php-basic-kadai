a860f79e69297ba9058bdd409d744299ed60b020
ryota0609 <komugi3@ymail.ne.jp>
2023-12-02 16:56:05 +0900

    連想配列の作成と値の出力

---
+++ associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+</head>
+
+<body>
+  <p>
+    <?php
+      //連想配列の作成と値の出力
+      $Array = ['name' => 'orion', 'price' => 200, 'weight' => 160];
+      print_r($Array);
+    ?>
+  </p>
+</body>
+
+</html>
